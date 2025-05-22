ddb1e0b6458ba4297e590cf5aa7f48c1e58b9e84
vrushmore123 <132755812+vrushmore123@users.noreply.github.com>
2025-05-23 00:32:17 +0530

    mongo link change

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -2,12 +2,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import ASCENDING
 
 try:
-    mongodb = AsyncIOMotorClient("mongodb://localhost:27017")
+    mongodb = AsyncIOMotorClient("mongodb+srv://morevrushali234:o5wvF2Amry4Q7FzY@courses.aw0aojx.mongodb.net/?retryWrites=true&w=majority&appName=Courses")
     db = mongodb.liahub_db  # Database name
     
     # Collections
     courses_collection = db.course
-    teacher_course_collection = db.teacher_courses  # Fixed variable name
+    teacher_course_collection = db.teacher_courses  
     profile_collection = db.profile
     
     print("MongoDB connection successful!")
